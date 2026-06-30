9f2ae713f3e8abce089802b9ddae9ee357ebfdb0
weicaixin <weicaixin@gmail.com>
2026-06-30 14:04:41 +0800

    Add feature-gated crate exports

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,21 @@ pub fn version() -> &'static str {
     env!("CARGO_PKG_VERSION")
 }
 
+#[cfg(feature = "asr")]
+pub use airs_asr as asr;
+
+#[cfg(feature = "audio")]
+pub use airs_audio as audio;
+
+#[cfg(feature = "hash")]
+pub use airs_hash as hash;
+
+#[cfg(feature = "image")]
+pub use airs_image as image;
+
+#[cfg(feature = "tts")]
+pub use airs_tts as tts;
+
 #[cfg(test)]
 mod tests {
     use super::*;
